a9ca60f2eec46785cd497989020fba7327f02150
loki <loki@macbook-loki.local>
2011-09-27 23:35:21 +0400

    Initial Commit

---
+++ Kontrafaktunet/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Kontrafaktunet
+//
+//  Created by loki on 27.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
