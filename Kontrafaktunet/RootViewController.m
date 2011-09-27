cd5c771b1a0e50c22632f3bc3126757b4592ccdd
loki <loki@macbook-loki.local>
2011-09-28 01:17:34 +0400

    in advance

--- Kontrafaktunet/RootViewController.m
+++ Kontrafaktunet/RootViewController.m
@@ -1,6 +1,27 @@
 #import "RootViewController.h"
+#import "RESTService.h"
+#import "WebParams.h"
+#import "Async.h"
 
 @implementation RootViewController
+
+#pragma mark private
+
+-(NSString*)code{
+    return [NSString stringWithFormat:@"%@%@%@%@", f1.text, f2.text, f3.text, f4.text];
+}
+
+-(id)checkCode{
+    RESTService *service = [[[RESTService alloc] initWithBaseUrl:@"http://code.slognosti.ru/api.ashx"] autorelease];
+    WebParams *params = [WebParams params];
+    [params addParam:@"check" forKey:@"command"];
+    [params addParam:self.code forKey:@"code"];
+    
+    NSError *error = nil;
+    id result = [service get:@"" withParams:params error:&error]; 
+    if(error) return  error;
+    return  result;
+}
 
 #pragma mark lifecycle
 
@@ -33,7 +54,7 @@
 #pragma mark events
 
 -(IBAction)onCheck{
-    
+
 }
 
 
